3caaa9b689a3c64add86e7c636c0c7c0669c4225
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-17 15:25:54 +0530

    inserting data into database

---
+++ 03_insert_into_users.sql
@@ -0,0 +1,11 @@
+use startersql;
+select * from users;
+
+-- insert into users values ('001', 'Anushka', 'Female', 'a@gmail.com', '2006-11-02', default);
+
+-- insert into users (name, email, gender, date_of_birth) values 
+-- ('BBB', 'b@gmail.com', 'Male', '2007-04-25'), 
+-- ('CCC', 'c@gmail.com', 'Female', '1963-11-15'), 
+-- ('DDD', 'd@gmail.com', 'Other', '2008-02-29'); 
+
+select * from users;
