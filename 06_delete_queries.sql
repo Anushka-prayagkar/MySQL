85cbc6814643aa22ae45bf2529deb9f4a6a36ab5
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-17 22:35:30 +0530

    learnt about delete queries

---
+++ 06_delete_queries.sql
@@ -0,0 +1,7 @@
+use startersql;
+
+-- delete from users where salary < 65000;
+-- delete from users where id = '7';
+-- delete from users where salary < 65000 and gender = 'Male';
+-- delete from users where salary < 50000 or date_of_birth > '1990-12-12';
+drop table users; 
