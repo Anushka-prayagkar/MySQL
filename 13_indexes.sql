87e5f3bf079fa9e4408bb1df82015d6e82fdfb49
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-20 16:52:40 +0530

    learnt about indexes in a database

---
+++ 13_indexes.sql
@@ -0,0 +1,11 @@
+use startersql;
+
+-- show indexes from users;
+
+-- create index idx_gender on users(gender);
+
+-- select email, gender from users where gender = 'Male';
+
+-- show indexes from users;
+
+drop index idx_email on users;
