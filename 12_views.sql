dc7ed42a1d9121be7666e49ed14c8edfd2a8a81e
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-20 16:42:12 +0530

    learnt about views

---
+++ 12_views.sql
@@ -0,0 +1,14 @@
+use startersql;
+
+-- create view rich_users as 
+-- select * from users where salary > 70000; 
+
+-- select * from rich_users;
+
+-- update users set salary = 30000 where id = 2;
+
+-- select * from rich_users;
+
+-- select name, email from rich_users;
+
+drop view rich_users;
